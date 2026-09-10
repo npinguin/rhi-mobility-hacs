2915bf11a426a184a5e26ed649dfee077578785b
npinguin <nicky.wessels@gmail.com>
2026-09-10 10:08:02 +0200

    M0.5.11: keep editable switches projected

--- custom_components/rhi_mobility/switch.py
+++ custom_components/rhi_mobility/switch.py
@@ -14,7 +14,6 @@ async def async_setup_entry(hass: HomeAssistant,entry: ConfigEntry,async_add_ent
         new=[]; wanted=set()
         for aid,asset in sorted(manager.assets.items()):
             for property_key,editable in editable_definitions(registry,asset.concept_id,'switch'):
-                if not is_available(manager,controller,aid,property_key,editable): continue
                 key=(aid,property_key); wanted.add(key)
                 if key not in created:
                     entity=MobilityCompatibilitySwitch(entry.entry_id,aid,property_key,editable,manager,controller); created[key]=entity; new.append(entity)
@@ -28,12 +27,8 @@ class MobilityCompatibilitySwitch(SwitchEntity):
     _attr_has_entity_name=True
     def __init__(self,entry_id,asset_id,property_key,editable,manager,controller):
         self.asset_id=asset_id; self.property_key=property_key; self.editable=editable; self.manager=manager; self.controller=controller
-        suffix=property_key.replace('.','_')
-        self._attr_unique_id=f'{DOMAIN}:{asset_id}:switch:{property_key}'; self._attr_suggested_object_id=f'{DOMAIN}_{asset_id}_{suffix}'
-        self._attr_name=editable.get('name') or property_key.split('.')[-1].replace('_',' ').title()
-        self._attr_device_info=logical_device_info(manager.hass,entry_id,manager,asset_id)
-    async def async_added_to_hass(self) -> None:
-        self.async_on_remove(self.manager.add_asset_listener(self.asset_id,self._changed))
+        suffix=property_key.replace('.','_'); self._attr_unique_id=f'{DOMAIN}:{asset_id}:switch:{property_key}'; self._attr_suggested_object_id=f'{DOMAIN}_{asset_id}_{suffix}'; self._attr_name=editable.get('name') or property_key.split('.')[-1].replace('_',' ').title(); self._attr_device_info=logical_device_info(manager.hass,entry_id,manager,asset_id)
+    async def async_added_to_hass(self): self.async_on_remove(self.manager.add_asset_listener(self.asset_id,self._changed))
     @callback
     def _changed(self): self.async_write_ha_state()
     @property
@@ -42,8 +37,7 @@ class MobilityCompatibilitySwitch(SwitchEntity):
     def available(self): return is_available(self.manager,self.controller,self.asset_id,self.property_key,self.editable)
     @property
     def extra_state_attributes(self):
-        if self.editable.get('write_kind')=='lifecycle_enabled_alias':
-            return {'compatibility_property':'asset.enabled','canonical_owner':'asset.lifecycle_status','second_lifecycle_owner':False}
+        if self.editable.get('write_kind')=='lifecycle_enabled_alias': return {'compatibility_property':'asset.enabled','canonical_owner':'asset.lifecycle_status','second_lifecycle_owner':False}
         return {'canonical_owner':self.property_key}
     async def async_turn_on(self,**kwargs): await async_write(self.manager,self.controller,self.asset_id,self.property_key,self.editable,True)
     async def async_turn_off(self,**kwargs): await async_write(self.manager,self.controller,self.asset_id,self.property_key,self.editable,False)
